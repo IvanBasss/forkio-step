eeeb67fa1dcd6f06ca95e9cbf7ef8be70d3bad0c
IvanBasss <122455382+IvanBasss@users.noreply.github.com>
2023-01-31 16:23:44 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const btnBurger = document.querySelector('.menu__burger');
+const btnBurgerArr = document.querySelectorAll('.header-icon');
+const menuList = document.querySelector('.menu__nav-bar');
+
+document.addEventListener('click', menu);
+function menu(event) {
+    if (event.target.closest('.menu__burger')) {
+        menuList.classList.toggle('active');
+        btnBurger.classList.toggle('active');
+    }
+    if (!event.target.closest('.menu__burger') & !event.target.closest('.menu__list')) {
+        menuList.classList.remove('active');
+        btnBurger.classList.remove('active');
+    }
+}
